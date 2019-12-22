1d39550239677d387c49ccdf0b93ceed6fc3c90f
gillchristian <gillchristiang@gmail.com>
2019-12-22 22:35:02 +0100

    feat(fmt): Unify union types in the same error
    Improves on #5

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,95 @@
 import * as array from 'fp-ts/lib/Array';
-import { fold } from 'fp-ts/lib/Either';
-import { map } from 'fp-ts/lib/Option';
+import { fold, mapLeft } from 'fp-ts/lib/Either';
+import * as NEA from 'fp-ts/lib/NonEmptyArray';
+import { map, none, some } from 'fp-ts/lib/Option';
 import { pipe } from 'fp-ts/lib/pipeable';
+import { toArray } from 'fp-ts/lib/Record';
 import * as t from 'io-ts';
 
-const jsToString = (value: t.mixed) => (value === undefined ? 'undefined' : JSON.stringify(value));
+const isUnionType = ({ type }: t.ContextEntry) => type instanceof t.UnionType;
 
-export const formatValidationError = (error: t.ValidationError) => {
-    const path = error.context
+const jsToString = (value: t.mixed) =>
+    value === undefined ? 'undefined' : JSON.stringify(value);
+
+const keyPath = (ctx: t.Context) =>
+    ctx
         .map(c => c.key)
-        // The context entry with an empty key is the original type ("default
-        // context"), not an type error.
+        // The context entry with an empty key is the original
+        // type ("default context"), not an type error.
         .filter(key => key.length > 0)
         .join('.');
 
-    // The actual error is last in context
-    const maybeErrorContext = array.last(
-        // https://github.com/gcanti/fp-ts/pull/544/files
-        error.context as Array<t.ContextEntry>
+// The actual error is last in context
+const getErrorFromCtx = ({ context }: t.ValidationError) =>
+    // https://github.com/gcanti/fp-ts/pull/544/files
+    array.last(context as Array<t.ContextEntry>);
+
+export const formatUnionError = (
+    errors: NEA.NonEmptyArray<t.ValidationError>,
+) => {
+    const error = NEA.head(errors);
+    const path = keyPath(error.context.filter(isUnionType));
+
+    const expectedTypes = pipe(
+        errors,
+        NEA.map(getErrorFromCtx),
+        arr => [NEA.head(arr), ...NEA.tail(arr)],
+        array.compact,
+        array.map(({ type }) => `    ${type.name}`),
+        arr => arr.join('\n'),
     );
 
-    return pipe(maybeErrorContext, map(errorContext => {
-        const expectedType = errorContext.type.name;
-        return (
-            // https://github.com/elm-lang/core/blob/18c9e84e975ed22649888bfad15d1efdb0128ab2/src/Native/Json.js#L199
-            // tslint:disable-next-line:prefer-template
-            `Expecting ${expectedType}`
-                + (path === '' ? '' : ` at ${path}`)
-                + ` but instead got: ${jsToString(error.value)}.`
-        );
-    }))
+    return expectedTypes.trim() === ''
+        ? none
+        : some(
+              // https://github.com/elm-lang/core/blob/18c9e84e975ed22649888bfad15d1efdb0128ab2/src/Native/Json.js#L199
+              // tslint:disable-next-line:prefer-template
+              `Expecting one of:\n${expectedTypes}` +
+                  (path === '' ? '\n' : `\nat ${path} `) +
+                  `but instead got: ${jsToString(error.value)}.`,
+          );
 };
 
-export const reporter = <T>(validation: t.Validation<T>) => (
-    pipe(validation, fold(errors => array.compact(errors.map(formatValidationError)), () => []))
+export const formatValidationError = (error: t.ValidationError) => {
+    const path = keyPath(error.context);
+
+    return pipe(
+        error,
+        getErrorFromCtx,
+        map(errorContext => {
+            const expectedType = errorContext.type.name;
+
+            return (
+                // https://github.com/elm-lang/core/blob/18c9e84e975ed22649888bfad15d1efdb0128ab2/src/Native/Json.js#L199
+                // tslint:disable-next-line:prefer-template
+                `Expecting ${expectedType}` +
+                (path === '' ? '' : ` at ${path}`) +
+                ` but instead got: ${jsToString(error.value)}.`
+            );
+        }),
+    );
+};
+
+export const format = (errors: NEA.NonEmptyArray<t.ValidationError>) =>
+    NEA.tail(errors).length > 0
+        ? formatUnionError(errors)
+        : formatValidationError(NEA.head(errors));
+
+const groupByKey = NEA.groupBy((error: t.ValidationError) =>
+    error.context.some(isUnionType)
+        ? keyPath(error.context.filter(isUnionType))
+        : keyPath(error.context),
 );
+
+export const reporter = <T>(validation: t.Validation<T>) =>
+    pipe(
+        validation,
+        mapLeft(groupByKey),
+        mapLeft(toArray),
+        mapLeft(array.map(([_key, errors]) => format(errors))),
+        mapLeft(array.compact),
+        fold(
+            errors => errors,
+            () => [],
+        ),
+    );
--- tests/index.ts
+++ tests/index.ts
@@ -41,3 +41,15 @@ const NumberGroups = t.array(t.array(t.number));
 
 logAndReport(NumberGroups.decode({}));
 logAndReport(NumberGroups.decode([[{}]]));
+
+const Unions = t.interface({
+    onOf: t.keyof({ a: null, b: null, c: null }),
+    stringUnion: t.union([t.literal('a'), t.literal('b'), t.literal('c')]),
+    interfaceUnion: t.union([
+        t.interface({ key: t.string }),
+        t.interface({ code: t.number }),
+    ]),
+});
+logAndReport(
+    Unions.decode({ oneOf: 'A', stringUnion: '', interfaceUnion: {} }),
+);
